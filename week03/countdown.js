8348241e234580c21a8a400391e4dc618c222553
luis <luiswoohoo@gmail.com>
2022-07-14 07:46:12 -0500

    Updated comments

--- week03/countdown.js
+++ week03/countdown.js
@@ -1,12 +1,12 @@
 function countdown(seconds) {
-    // Immediately print starting amount of seconds
+    // Immediately print starting amount of seconds.
     console.log(seconds);
 
-    // Wait a second before printing each subsequent amount
+    // Wait a second before printing each subsequent amount.
     // setInterval is basically a loop that keeps running on
-    // a timer until clearInterval is called
+    // a timer until clearInterval is called.
     let intervalID = setInterval(
-        // setInterval takes a callback function
+        // setInterval takes a callback function.
         // Once the timer expires these functions are put into the 
         // callback queue waiting to 
         // be executed once the main stack is empty.
