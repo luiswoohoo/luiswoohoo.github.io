496dfdf18d87a6101be536deb37c8a3c0a5cf37c
luis <luiswoohoo@gmail.com>
2022-08-08 15:38:48 -0500

    Updated html, css, js, favicon, image

--- Kiti_Color_Site/color.js
+++ Kiti_Color_Site/color.js
@@ -1,2 +1,6 @@
-// let color = document.getElementById('user-color');
-// console.log(color);
+const colorInput = document.getElementById("color-input")
+
+colorInput.addEventListener("change", function(event) {
+    let color = event.target.value
+    document.getElementById('container').style.backgroundColor = color;document.getElementById('title').style.color = color;
+})
