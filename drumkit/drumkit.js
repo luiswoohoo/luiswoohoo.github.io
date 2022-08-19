c1227822cc257175388b8023f29deb9a4830b8aa
luis <luiswoohoo@gmail.com>
2022-08-18 19:24:21 -0500

    mvp

---
+++ drumkit/drumkit.js
@@ -0,0 +1,39 @@
+
+function playSound(e) {
+    let audio
+    let key
+
+    if (e.type === 'click') {
+        audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`)
+        key = document.querySelector(`.key[data-key="${e.target.dataset.key}"]`)
+    } else {
+        audio = document.querySelector(`audio[data-key="${e.code}"]`)
+        key = document.querySelector(`.key[data-key="${e.code}"]`)
+    }
+    // console.log(e)
+    
+    // const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+    // const key = document.querySelector(`.key[data-key="${e.code}"]`)
+
+    if (!audio) return //stop function from running
+    audio.currentTime = 0 //start at beginning of audio file
+    audio.play()
+    key.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+for (const key of keys) {
+    key.addEventListener('transitionend', removeTransition)
+    key.addEventListener('click', playSound)
+}
+// This does same as the above for of loop
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+
+window.addEventListener('keydown', playSound)
+
